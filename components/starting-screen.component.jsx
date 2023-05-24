2c5e08ab935875566f543322f17c47684d7f05ad
Christine Yoo <cyoo@zixcorp.com>
2023-05-23 22:29:29 -0500

    added background image for start screen

--- components/starting-screen.component.jsx
+++ components/starting-screen.component.jsx
@@ -1,18 +1,19 @@
-import { Button, StyleSheet } from 'react-native'
+import { Text, View , Button, StyleSheet } from 'react-native'
 import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native'
+import { ImageBackground } from 'react-native-web'
 
-const StartingScreen = ( { navigation } ) => {
+const StartingScreen = ({ navigation }) => {
+  const image = require('../assets/dog-and-owner-outdoors.jpeg')
   return (
     <View style={styles.container}>
-      <Text>Welcome to PetSpace!</Text>
-      <StatusBar style='auto' />
-      <Button
-        title="Let's get started!"
-        onPress={() =>
-          navigation.navigate('Discover', { petName: 'Fido'})
-        }
-      />
+      <ImageBackground source={image} resizeMode='cover' style={styles.image}>
+        <Text style={styles.text}>Welcome to PetSpace!</Text>
+        <StatusBar style='auto' />
+        <Button
+          title="Let's get started!"
+          onPress={() => navigation.navigate('Discover', { petName: 'Fido' })}
+        />
+      </ImageBackground>
     </View>
   )
 }
@@ -21,8 +22,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+  image: {
+    flex: 1,
     justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    marginBottom: '75px',
+    fontSize: '2em',
+    color: 'white'
   },
 })
 
